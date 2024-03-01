b80cee2a21fd42f67614850a4adadc5f8df99d86
Gabriel Sabérian <2242462@edu.vaniercollege.qc.ca>
2024-03-01 09:46:16 -0500

    End of class commit

--- src/main/java/edu/vanier/physics/Momentum.java
+++ src/main/java/edu/vanier/physics/Momentum.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.vanier.physics;
 
 /**
  *
  * @author 2242462
  */
+
+
+
 public class Momentum {
+    
+   private static final float mass = 10.0f;
+   
+   
     
 }
