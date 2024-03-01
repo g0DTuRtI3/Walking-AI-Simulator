ccca7ddfda88b48fdc9607d3d3ca5e07510caa6b
Youssef Ben Mouny <2248826@edu.vaniercollege.qc.ca>
2024-03-01 09:48:10 -0500

    gravity added

---
+++ src/main/java/edu/vanier/physics/Gravity.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.vanier.physics;
+
+/**
+ *
+ * @author 2248826
+ */
+public class Gravity {
+    private double acceleration = 9.80;
+    public double getAcceleration(){
+    return this.acceleration;
+    }
+    public void setAcceleration(double acceleration){
+    this.acceleration = acceleration;
+    }
+}
