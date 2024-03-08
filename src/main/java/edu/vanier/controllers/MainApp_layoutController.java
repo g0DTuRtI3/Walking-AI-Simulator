56d8fc98bcc890c809dbd34b821c7605d5b25a7c
Ze Yu Huang <2253883@edu.vaniercollege.qc.ca>
2024-03-08 09:45:19 -0500

    broken need to fix the run

---
+++ src/main/java/edu/vanier/controllers/MainApp_layoutController.java
@@ -0,0 +1,31 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainApp_layoutController {
+
+    @FXML
+    private Button btn_AboutUs;
+
+    @FXML
+    private Button btn_Settings;
+
+    @FXML
+    private Button btn_Simulation;
+
+    @FXML
+    void AboutUsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SimulationOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void settingOnAction(ActionEvent event) {
+
+    }
+
+}
