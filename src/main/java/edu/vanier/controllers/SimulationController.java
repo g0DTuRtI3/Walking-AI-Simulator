868844fe3c6bda39376ead93ebc1918a4a11055a
Gabriel Saberian/ <gabsab1000@gmail.com>
2024-03-11 21:58:09 -0700

    Fixed fxml

--- src/main/java/edu/vanier/controllers/SimulationController.java
+++ src/main/java/edu/vanier/controllers/SimulationController.java
@@ -9,7 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
-public class Simulation_layoutController {
+public class SimulationController {
 
     @FXML
     private Pane SimulationPane;
