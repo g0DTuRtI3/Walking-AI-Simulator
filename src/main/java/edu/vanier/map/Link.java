183253309fad38d366c7f5b5e82df52ea3161cca
Youssef Ben Mouny <2248826@edu.vaniercollege.qc.ca>
2024-03-19 13:50:36 -0400

    angle

--- src/main/java/edu/vanier/map/Link.java
+++ src/main/java/edu/vanier/map/Link.java
@@ -11,26 +11,44 @@ import javafx.scene.paint.Color;
  * @author YOUSSEF
  */
 public class Link extends javafx.scene.shape.Line {
-    
+
     private static double strokeWidth = 5;
-    
+
     private Node prevNode;
     private Node nextNode;
-    
+
     public Link(Node prevNode, Node nextNode, Color colorOfLine) {
         super(prevNode.getCenterX(), prevNode.getCenterY(), nextNode.getCenterX(), nextNode.getCenterY());
         super.setStrokeWidth(strokeWidth);
         super.setStroke(colorOfLine);
-        
+
     }
-    
+
     public void updateLink() {
         super.setStartX(prevNode.getCenterX());
         super.setStartY(prevNode.getCenterY());
         super.setEndX(nextNode.getCenterX());
         super.setEndY(nextNode.getCenterY());
     }
-    public double getRadAngle(){
-        return 0;
+
+    public double getRadAngle() {
+        double adjacent = nextNode.getTranslateX() - prevNode.getTranslateX();
+        double opposite = nextNode.getTranslateY() - nextNode.getTranslateY();
+        double angleRad = 0;
+        try {
+            angleRad = Math.atan(Math.abs(opposite / adjacent));
+        } catch (Exception e) {
+            angleRad = Math.PI / 2;
+        }
+
+        if (adjacent < 0 || opposite < 0) {
+            return angleRad + Math.PI;
+        } else if (adjacent < 0) {
+            return Math.PI - angleRad;
+        } else if (opposite < 0) {
+            return (2 * Math.PI) - angleRad;
+        }
+        
+        return angleRad;
     }
 }
