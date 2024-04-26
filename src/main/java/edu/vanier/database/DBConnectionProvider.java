7ba65a2567168997e784c7fa488f13f901bb4390
Ze Yu Huang <2253883@edu.vaniercollege.qc.ca>
2024-04-26 08:15:24 -0400

    save bfore merge

---
+++ src/main/java/edu/vanier/database/DBConnectionProvider.java
@@ -0,0 +1,37 @@
+package edu.vanier.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Zeyu Huang
+ * REF:https://github.com/frostybee/fx-gallery
+ */
+
+/**
+ * A singleton class that enables connection to an SQLite database.
+ */
+public abstract class DBConnectionProvider {
+
+    /**
+     * Opens a connection to an SQLite database.
+     *
+     * @param databaseName the name of the SQLite database file (it can also be
+     * a relative path.
+     * @return an opened connection to an SQLite database.
+     */
+    public Connection getConnection(String databaseName) {
+        try {
+            String db_location = DBConnectionProvider.class.getResource("/data/" + databaseName).toExternalForm();
+            Class.forName("org.sqlite.JDBC");
+            Connection dbConnection = DriverManager.getConnection("jdbc:sqlite:" + db_location);
+            return dbConnection;
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
+
