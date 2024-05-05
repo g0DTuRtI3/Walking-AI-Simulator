4715ae399e6dd76f707593a845918f535d64e9a0
Zeyu Huang <zeze33629@gmail.com>
2024-05-05 19:31:36 -0400

    added delete from database

--- src/main/java/edu/vanier/controllers/loadController.java
+++ src/main/java/edu/vanier/controllers/loadController.java
@@ -3,9 +3,13 @@ package edu.vanier.controllers;
 import edu.vanier.database.SqliteDB;
 import java.io.IOException;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -15,6 +19,9 @@ public class loadController {
     private final SqliteDB database = new SqliteDB();
     private final Stage primaryStage;
 
+    @FXML
+    private ChoiceBox<String> cb_model;
+    
     @FXML
     private ListView<String> modelList;
     
@@ -38,10 +45,44 @@ public class loadController {
                 System.out.println(ex);
             }
         });
+        
+        cb_model.getItems().addAll(database.readModelName());
+    }
+    
+    @FXML
+    void deleteOnAction(ActionEvent event) {
+        if (cb_model.getValue() != null) {
+            System.out.println("Deleting: " + cb_model.getValue());
+            database.deleteModel(cb_model.getValue());
+            
+            modelList.getItems().clear();
+            modelList.getItems().addAll(database.readModelName());
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Please chose a medel to delete");
+            alert.show();
+        }
+    }
+    
+    @FXML
+    void returnOnAction(ActionEvent event) throws IOException {
+        FXMLLoader mainAppLoader = new FXMLLoader(getClass().getResource("/fxml/Editor_layout.fxml"));
+        EditorController controller = new EditorController(primaryStage);
+        mainAppLoader.setController(controller);
+        Pane root = mainAppLoader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setMaximized(false);
+        primaryStage.setMaximized(true);
+        // We just need to bring the main window to front.
+        primaryStage.setAlwaysOnTop(true);
+        primaryStage.setTitle("Editor");
+        primaryStage.show();
     }
 
     /**
-     * The method switches the scene to the Editor.
+     * The method switches the scene to the Editor. It makes the editor load the selected walker.
      * 
      * @param selectedModel The name of the model that the user chose.
      * @throws IOException 
@@ -61,4 +102,5 @@ public class loadController {
         primaryStage.setTitle("Editor");
         primaryStage.show();
     }
+    
 }
